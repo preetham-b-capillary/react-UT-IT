f5a831ecc0151a83711289de207edb68529e9b61
Preetham <preetham.b@capillarytech.com>
2024-02-01 00:01:03 +0530

    todo

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Write test case for todo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  /*
+    check if elements renders and button, input box is present
+    stimulate add todo
+    on success assert on notification and check if inserted data present in ui
+    if failure assert failure notification and new row should not be created
+    if delete button clicked assert on notification and check if deleted data is not present in ui
+  */
 });
