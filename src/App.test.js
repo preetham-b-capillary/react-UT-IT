8a5c0a366e16aff91418b9785a6d1ed9190cd1ef
Akash Verma <akash.verma@capillarytech.com>
2024-02-01 14:40:04 +0530

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,84 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("Test counter App", () => {
+test("assert if initial count value is 0 on render", () => {
   render(<App />);
-  /*
-  TODO:
-    assert if initial count value is 0 on render
-    assert if increment button is present
-    assert if decrement button is present
-    click increment button twice
-    assert if count value is 2
-    click decrement button once
-    assert if count value is 1
-    click decrement button once
-    assert if count value is 0
-    click decrement button once
-    assert if count value is 0 if below 0 it wont decrement
-
-    hints:
-    for getting dom elements use getBy/queryBy from screen
-    use getByRole/findByRole query to find 'increment' button
-    use fireevent or user event to click
-    make assertions using expect with screen.getBy or queryBy
-    repeat the above steps dependent on test case requirement in todo
-  */
+  const countValue = screen.getByText(/counter/i)
+  expect(countValue).toHaveTextContent(/counter value is 0/i);  
 });
+
+test("increment button is present", ()=>{
+  render(<App />);
+  const incrementButton = screen.getByRole('button', {
+    name: /increment/i
+  })
+  expect(incrementButton).toBeInTheDocument();
+})
+
+test("decrement button is present", ()=>{
+  render(<App />);
+  const decrementButton = screen.getByRole('button', {
+    name: /decrement/i
+  })
+  expect(decrementButton).toBeInTheDocument();
+})
+
+test("click increment button twice assert if count value is 2", ()=>{
+  render(<App />);
+  const incrementButton = screen.getByRole('button', {
+    name: /increment/i
+  })
+  fireEvent.click(incrementButton);
+  fireEvent.click(incrementButton);
+  const countValue = screen.getByText(/counter/i)
+  expect(countValue).toHaveTextContent(/counter value is 2/i);  
+})
+
+test("click decrement button once assert if count value is 1", ()=>{
+  render(<App />);
+  const incrementButton = screen.getByRole('button', {
+    name: /increment/i
+  })
+  fireEvent.click(incrementButton);
+  fireEvent.click(incrementButton);
+  const decrementButton = screen.getByRole('button', {
+    name: /decrement/i
+  })
+  fireEvent.click(decrementButton);
+  const countValue = screen.getByText(/counter/i)
+  expect(countValue).toHaveTextContent(/counter value is 1/i);  
+})
+
+test("click decrement button once assert if count value is 0", ()=>{
+  render(<App />);
+  const incrementButton = screen.getByRole('button', {
+    name: /increment/i
+  })
+  fireEvent.click(incrementButton);
+  fireEvent.click(incrementButton);
+  const decrementButton = screen.getByRole('button', {
+    name: /decrement/i
+  })
+  fireEvent.click(decrementButton);
+  fireEvent.click(decrementButton);
+  const countValue = screen.getByText(/counter/i)
+  expect(countValue).toHaveTextContent(/counter value is 0/i);  
+})
+
+test("click decrement button once assert if count value is 0 if below 0 it wont decrement", ()=>{
+  render(<App />);
+  const incrementButton = screen.getByRole('button', {
+    name: /increment/i
+  })
+  fireEvent.click(incrementButton);
+  fireEvent.click(incrementButton);
+  const decrementButton = screen.getByRole('button', {
+    name: /decrement/i
+  })
+  fireEvent.click(decrementButton);
+  fireEvent.click(decrementButton);
+  fireEvent.click(decrementButton);
+  const countValue = screen.getByText(/counter/i)
+  expect(countValue).toHaveTextContent(/counter value is 0/i);  
+})
+
