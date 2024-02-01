77ce8f45de59c07d9896a923ea73d62dd11ca410
Nandini P <nandini.palakollu@capillarytech.com>
2024-02-01 17:25:06 +0530

    starter-added testcases

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,44 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
 test("Stimulate login flow", () => {
   render(<App />);
-  /*
-   TODO:
-    1. Fill in the username and password fields getByLabelText
-    2. type user name and password - fireEvent.change or userEvent.type
-    3. find the submit button - getByRole/findByRole
-    4. click the submit button - fireEvent.click or userEvent.click
-    5. add a assertion to check if submitted data is correct
-    6. mock onSubmit function to the App component and check if its getting called with correct username and password
-  */
+  // 1. Fill in the username and password fields getByLabelText
+  const userNameInput = screen.getByLabelText("Username");
+  const pwdInput = screen.getByLabelText("Password");
+
+  userEvent.type(userNameInput, "Nandini");
+  userEvent.type(pwdInput, "12345678");
+
+  expect(userNameInput.value).toBe("Nandini");
+  expect(pwdInput.value).toBe("12345678");
+
+  // 2. type username and password - fireEvent.change or userEvent.type
+  fireEvent.change(userNameInput, { target: { value: "Nandini Palakollu" } });
+  fireEvent.change(pwdInput, { target: { value: "12345678" } });
+
+  // 3. find the submit button - getByRole/findByRole
+  const submitButton=screen.getByRole('button',{name:'Submit'});
+  expect(submitButton).toBeInTheDocument();
+
+  // 4. click the submit button - fireEvent.click or userEvent.click
+  fireEvent.click(submitButton);
+
+  // 5. add an assertion to check if submitted data is correct
+  // const mockSubmitFunc=jest.fn();
+  // expect(mockSubmitFunc).toHaveBeenCalledWith({
+  //   username:"Nandini Palakollu",
+  //   password:"12345678"
+  // });
 });
+
+/*
+  TODO:
+  1. Fill in the username and password fields getByLabelText
+  2. type user name and password - fireEvent.change or userEvent.type
+  3. find the submit button - getByRole/findByRole
+  4. click the submit button - fireEvent.click or userEvent.click
+  5. add a assertion to check if submitted data is correct
+  6. mock onSubmit function to the App component and check if its getting called with correct username and password
+*/
