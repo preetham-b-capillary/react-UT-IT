a5d3b10763f3a7f4118d01d2fec7c71394a462af
Akash Verma <akash.verma@capillarytech.com>
2024-02-01 17:31:49 +0530

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
-
-test('Write test case for todo', () => {
-  render(<App />);
-  /*
+ /*
     check if elements renders and button, input box is present
     stimulate add todo
     on success assert on notification and check if inserted data present in ui
     if failure assert failure notification and new row should not be created
     if delete button clicked assert on notification and check if deleted data is not present in ui
   */
+test('Write test case for todo', () => {
+  render(<App />);
+  const addTodoButton = screen.getByRole('button', {
+    name: /add todo/i
+  });
+  const textArea = screen.getByRole('textbox');
+  expect(addTodoButton).toBeInTheDocument();
+  expect(textArea).toBeInTheDocument();
 });
