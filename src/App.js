cf41b0348c10ade4cfb20a769e6e49679d42c86f
Preetham <preetham.b@capillarytech.com>
2024-02-02 11:18:49 +0530

    overwriting issue fix

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h2>Hi im overwriting this component</h2>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
