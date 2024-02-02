2eebe9e1bb0dd79193b50cf985d8d31478ead6d6
BhavikDevganiya-capillary <bhavikdevganiya@CLPT1009.local>
2024-02-02 13:30:48 +0530

    testing on UI and Functions

--- src/App.test.js
+++ src/App.test.js
@@ -3,20 +3,41 @@ import App from "./App";
 
 test("Test counter App", () => {
   render(<App />);
-  /*
-  TODO:
-    assert if initial count value is 0 on render
-    assert if increment button is present
-    assert if decrement button is present
-    click increment button twice
-    assert if count value is 2
-    click decrement button once
-    assert if count value is 1
-    click decrement button once
-    assert if count value is 0
-    click decrement button once
-    assert if count value is 0 if below 0 it wont decrement
+  //TODO:
+  //assert if initial count value is 0 on render
+  const currCount = screen.getByTestId("counter");
 
+  expect(currCount).toHaveTextContent(0);
+
+  //assert if increment button is present
+  const incrementButton = screen.getByRole("button", {
+    name: /increment/i,
+  });
+  expect(incrementButton).toBeInTheDocument();
+  //assert if decrement button is present
+  const decrementButton = screen.getByRole("button", {
+    name: /decrement/i,
+  });
+  expect(decrementButton).toBeInTheDocument();
+  //click increment button twice
+  fireEvent.click(incrementButton);
+  fireEvent.click(incrementButton);
+  //assert if count value is 2
+  expect(currCount).toHaveTextContent(2);
+  //click decrement button once
+  fireEvent.click(decrementButton);
+  //assert if count value is 1
+  expect(currCount).toHaveTextContent(1);
+  //click decrement button once
+  fireEvent.click(decrementButton);
+  //assert if count value is 0
+  expect(currCount).toHaveTextContent(0);
+  //click decrement button once
+  fireEvent.click(decrementButton);
+  //assert if count value is 0 if below 0 it wont decrement
+  expect(currCount).toHaveTextContent(0);
+  screen.debug(null, Infinity);
+  /*
     hints:
     for getting dom elements use getBy/queryBy from screen
     use getByRole/findByRole query to find 'increment' button
